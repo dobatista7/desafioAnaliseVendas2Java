0edb1218ac00180756aed9dc879673db7c9d9f44
Douglas Batista <dobatista7@gmail.com>
2024-09-03 22:12:08 -0300

    Desenvolvimento  do Map no programa principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,22 @@
 package application;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entre o caminho do arquivo: ");
+        String path = sc.nextLine();  // Leitura do caminho do arquivo pelo usuário
+        System.out.println();
+
+        // Armazenar as instâncias de Sale, onde a chave é o nome do vendedor
+        Map<String, Double> salesMap = new HashMap<>();
+
 
         }
     }
